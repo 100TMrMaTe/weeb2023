f6655758c83ee7d9e58f1d218624864a4bf44467
Konyhási Máté <konyhasim21@ADY-NAGYATAD.SULINET.HU>
2023-09-15 13:20:42 +0200

    bal

---
+++ c#/Új mappa/gyak3/gyak3/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.Write("kérek egy szamot: ");
+int szam = Convert.ToInt32(Console.ReadLine());
+
+for(int i=0; i<szam; i++)
+{
+    Console.Write(i*10+", ");
+}
+int a = 0;
+for (int i = 1; i <= szam; i++)
+{
+    for (int k = 1; k <= szam; k++)
+    {
+        Console.Write(i * k+" \t");
+    }
+    Console.WriteLine();
+}
+*/
+/*
+int a = 0;
+
+for(int i=0;i<a;i++)
+{
+    a += 1;
+    Console.WriteLine(i);
+    
+}
+*/
+try
+{
+    Console.Write("kérek egy  2 jegyu szamot: ");
+    int szam = Convert.ToInt32(Console.ReadLine());
+}
+catch
+
+
+
+
+
+
+
+
+
+
