736f1c092e2230b474c229ed94569b5f3226e42e
Konyhási Máté <konyhasim21@ADY-NAGYATAD.SULINET.HU>
2023-09-27 13:59:50 +0200

    s

---
+++ c#/hf4/hf4/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Console.Write("kérek egy oldalt: ");
+double szam= Convert.ToDouble(Console.ReadLine());
+
+Console.Write("kérek egy oldalt: ");
+double szam1 = Convert.ToDouble(Console.ReadLine());
+
+
+szam = szam * szam;
+szam1 = szam1 * szam1;
+Console.WriteLine("c= "+Math.Sqrt(szam+szam1));
+*/
+
+for(int i=1;i<100;i++)
+{
+    for (int a = 1; a < 100; a++)
+    {
+        i = i * i;
+        a = a * a;
+        double c = Math.Sqrt(i + a);
+        if (c%1==0)
+        {
+            Console.WriteLine("a= "+i);
+            Console.WriteLine("b= " + a);
+            Console.WriteLine("c= " + c);
+        }
+    }
+}
