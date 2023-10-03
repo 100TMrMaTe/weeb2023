c55986574880cf2f79e4b257300c5d5536cec91e
Konyhási Máté <konyhasim21@ADY-NAGYATAD.SULINET.HU>
2023-10-03 09:40:06 +0200

    b

---
+++ c#/gyak8/gyak8/gyak8/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+bool jo = false;
+while (!jo)
+{
+    jo = true;
+    Console.Write("mit váltsak át PL;12 cm :");
+    string be = Console.ReadLine();
+    string[] a = be.Split(' ');
+    string[] valtoz = { "g", "dkg", "kg" };
+    if (a.Length != 2)
+    {
+        jo= false;
+        continue;
+    }
+    double szam = 0;
+    try
+    {
+        szam = Convert.ToDouble(a[0]);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("nem jo  formatum!");
+        continue;
+    }
+    Console.WriteLine(szam);
+}
