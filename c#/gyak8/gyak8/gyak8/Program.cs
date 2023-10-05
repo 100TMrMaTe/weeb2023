52a5ab9367757bd773ba91c89f6a5854132a20ec
Konyhási Máté <konyhasim21@ADY-NAGYATAD.SULINET.HU>
2023-10-05 08:44:52 +0200

    b

--- c#/gyak8/gyak8/gyak8/Program.cs
+++ c#/gyak8/gyak8/gyak8/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+string[] hoszusag = { "mm", "cm", "dm", "m", "km" };
 bool jo = false;
 while (!jo)
 {
@@ -22,7 +22,24 @@ while (!jo)
     catch (Exception)
     {
         Console.WriteLine("nem jo  formatum!");
+        jo = false;
         continue;
     }
     Console.WriteLine(szam);
+    if (hoszusag.Contains(a[1]))
+    {
+    }
+    else
+    {
+        Console.WriteLine("nem jo mertek egyseg");
+        jo = false;
+        continue;
+    }
+    Console.WriteLine("mire valtsam at: ");
+    for (int i = 0; i < hoszusag.Length; i++)
+    {
+        Console.WriteLine(hoszusag[i]);
+    }
+    Console.Write("valassz: ");
+    string valasz=Console.ReadLine();d
 }
