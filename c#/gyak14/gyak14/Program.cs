917dd0ff02e05dc1e8dcc3767c99e565cc31d896
Konyhási Máté <konyhasim21@ADY-NAGYATAD.SULINET.HU>
2023-11-30 09:40:08 +0100

    as

---
+++ c#/gyak14/gyak14/Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> szavak= new List<string>();
+
+szavak.Add("alma");
+szavak.Add("korte");
+szavak.Add("barack");
+szavak.Add("ananasz");
+
+
+
+for (int i = 0; i < szavak.Count; i++)
+{
+    Console.WriteLine(szavak[i]);
+}
+
+foreach(string s in szavak)
+{
+    Console.WriteLine(s);
+}
+
+Console.WriteLine(szavak.Contains("korte"));
+Dictionary<string,int> p= new Dictionary<string,int>();
+
+p.Add("laci", 3);
+p.Add("bence", 0);
+Console.WriteLine(p.Count);
