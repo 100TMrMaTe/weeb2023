913fefe408896c129c5c616797e756ebe92ef343
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2023-12-24 15:31:25 +0100

    Add files via upload

---
+++ c#/pulzus 0353/pulzus 0353/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("életkor: ");
+int eletkor = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("nyugalmi pulzus: ");
+int nyugp=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("hany % szeretne edzeni: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+int maxp = 220 - eletkor;
+int pult = maxp - nyugp;
+
+double ajanlott = pult * (a / 100.0) + nyugp;
+Console.WriteLine(Math.Round(ajanlott)+" a pulzus amivel ajánlott edzeni "+a+"% intenzitás mellet.");
+
+
+
+
