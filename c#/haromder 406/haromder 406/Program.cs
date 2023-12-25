13e80609f5deaf0d6ba03b9bfa84719c0589e4a4
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2023-12-25 14:58:46 +0100

    Add files via upload

---
+++ c#/haromder 406/haromder 406/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+Console.Write("a: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("b: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.Write("c: ");
+int c=Convert.ToInt32(Console.ReadLine());
+
+int kerulet = 0;
+while (true) 
+{
+    if (a + b > c || b + c > a || c + a > b)
+    {
+        
+    }
+    else
+    {
+        Console.WriteLine("nem szerkesztheto haromszog!");
+        break;
+    }
+    kerulet=a+b+c;
+    break;
+    
+}
+Console.WriteLine("haromszog kerulete: "+kerulet);
