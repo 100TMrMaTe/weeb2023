0bd374d3d9b28746ee5e461097a6293ae1c5454e
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2023-12-25 13:32:32 +0100

    Add files via upload

---
+++ c#/repak 0376/repak 0376/Program.cs
@@ -0,0 +1,58 @@
+﻿//1
+
+List<int> szamok = new List<int>();
+
+Console.Write("1. beszalitas: ");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+Console.Write("2. beszalitas: ");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+Console.Write("3. beszalitas: ");
+szamok.Add(Convert.ToInt32(Console.ReadLine()));
+
+for(int i = 0; i < szamok.Count; i++)
+{
+    while (szamok[i] < 20 || szamok[i] > 150)
+    {
+        Console.Write((i+1)+". beszalitas: ");
+        szamok[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+double avg = szamok.Average();
+int ossz=szamok.Sum();
+Console.WriteLine(Math.Round(avg,2)+"Avg "+ossz+"T");
+
+static List<int> szamok1()
+{
+    List<int> szamok = new List<int>();
+
+    Console.Write("1. beszalitas: ");
+    szamok.Add(Convert.ToInt32(Console.ReadLine()));
+    Console.Write("2. beszalitas: ");
+    szamok.Add(Convert.ToInt32(Console.ReadLine()));
+    Console.Write("3. beszalitas: ");
+    szamok.Add(Convert.ToInt32(Console.ReadLine()));
+
+    for (int i = 0; i < szamok.Count; i++)
+    {
+        while (szamok[i] < 20 || szamok[i] > 150)
+        {
+            Console.Write((i + 1) + ". beszalitas: ");
+            szamok[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    return szamok;
+}
+
+List<int> szamok2 = szamok1();
+
+int a = 0;
+double b = 0.0;
+for (int i = 0;i < szamok2.Count;i++)
+{
+    a+= szamok2[i];
+    b+= szamok2[i];
+}
+b = b/szamok2.Count;
+
+Console.WriteLine(Math.Round(b,2)+"Avg "+a+"T");
