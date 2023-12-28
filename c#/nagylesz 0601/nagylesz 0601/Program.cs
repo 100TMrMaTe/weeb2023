f28ce11a54b17e0ed5d626c1d9f8f97170d4f180
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2023-12-28 22:46:19 +0100

    Add files via upload

---
+++ c#/nagylesz 0601/nagylesz 0601/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+//1.
+
+Console.Write("kérek egy kisbetut: ");
+string w=Console.ReadLine();
+
+string s = "qwertzuioplkjhgfdsayxcvbnmáéűúőóüöí-";
+string q=s.ToUpper();
+
+for(int i = 0;i < s.Length;i++)
+{
+    if (s.IndexOf(w) != -1 && w.Length < 2)
+    {
+        break;
+    }
+    else
+    {
+        Console.Write("adjon meg egy kisbetus karakter!: ");
+        w= Console.ReadLine();
+    }
+}
+int c=s.IndexOf(w);
+
+Console.WriteLine(q[c]);
+
+
+
+
+
+//2.
+
+
+
+
+Console.Write("kérek egy kisbetut: ");
+string a =Console.ReadLine();
+
+bool isValid(string str)
+{
+    return Regex.IsMatch(str, @"[a-z]");
+}
+
+while (true)
+{
+    if (isValid(a) == true && a.Length<2)
+    {
+        break;
+    }
+    else
+    {
+        Console.Write("adjon meg egy kisbetut!: ");
+        a = Console.ReadLine();
+    }
+      
+}
+a = char.ToUpper(a[0]) + a.Substring(1);
+Console.WriteLine(a);
+
+
+//3.
+
+Console.Write("kérek egy kisbetut: ");
+char b = Console.ReadKey().KeyChar;
+Console.WriteLine();
+
+
+while (true)
+{
+    if (char.IsLower(b))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("not an Uppercase Letter ");
+        Console.Write("kérek egy kisbetut: ");
+        b = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine(b.ToString().ToUpper());
