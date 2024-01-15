05fbac0896fe70ca8845a45f4261d79f7e81ac67
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2024-01-15 23:53:23 +0100

    Add files via upload

---
+++ c#/306/306/Program.cs
@@ -0,0 +1,17 @@
+﻿//1
+Console.Write("kérem a haromszog hosszat: ");
+int a=Int32.Parse(Console.ReadLine());
+
+Console.Write("kérem a haromszog magassagat: ");
+int b = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("haromszog terulete= "+a/2*b);
+//2
+
+Console.Write("kérem a haromszog hosszat: ");
+int c = int.Parse(Console.ReadLine());
+
+Console.Write("kérem a haromszog hosszat: ");
+int d = int.Parse(Console.ReadLine());
+
+Console.WriteLine("haromszog terulete= "+c*d/2);
