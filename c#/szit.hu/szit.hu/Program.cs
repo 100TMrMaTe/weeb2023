ed739eb91970df3ab2e75adccad2b51892e5c8e1
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2024-01-15 13:46:13 +0100

    Add files via upload

---
+++ c#/szit.hu/szit.hu/Program.cs
@@ -0,0 +1,16 @@
+﻿//702
+//1
+using System.Data;
+
+double[] a = new double[] { 3, 7.12,15.65,-80,50,57 };
+
+double n = 0;
+for (int i = 0; i < a.Length; i++)
+    {
+        if (n < a[i])
+        {
+            n = a[i];
+        }
+    }
+Console.WriteLine("legnagyobb szam: "+n);
+
