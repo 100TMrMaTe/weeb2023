27c911f8adc0b976fdb310db2d178942787385ba
100TMrMaTe <98027630+100TMrMaTe@users.noreply.github.com>
2024-01-16 21:10:00 +0100

    Update Program.cs

--- c#/306/306/Program.cs
+++ c#/306/306/Program.cs
@@ -1,17 +1,17 @@
 ﻿//1
-Console.Write("kérem a haromszog hosszat: ");
+Console.Write("kérem a haromszog eggyik oldalat: ");
 int a=Int32.Parse(Console.ReadLine());
 
-Console.Write("kérem a haromszog magassagat: ");
+Console.Write("kérem az elozo megadott oldalhoz tartozo magassagot: ");
 int b = Int32.Parse(Console.ReadLine());
 
 Console.WriteLine("haromszog terulete= "+a/2*b);
 //2
 
-Console.Write("kérem a haromszog hosszat: ");
+Console.Write("kérem a haromszog eggyik oldalat: ");
 int c = int.Parse(Console.ReadLine());
 
-Console.Write("kérem a haromszog hosszat: ");
+Console.Write("kérem az elozo megadott oldalhoz tartozo magassagot: ");
 int d = int.Parse(Console.ReadLine());
 
 Console.WriteLine("haromszog terulete= "+c*d/2);
